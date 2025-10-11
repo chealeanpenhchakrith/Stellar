50afd7650834daba0feefcfa6474a98bb61814cf
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-10-11 12:05:09 +0200

    Add a background image to the home page with a hero section

--- nasa/src/pages/Home.tsx
+++ nasa/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-[url('../src/assets/ImageBackground.png')] bg-cover bg-center h-screen"></div>
+      <div className="bg-[url('../src/assets/ImageBackground.png')] bg-cover bg-center h-screen relative">
+        <div className="flex flex-col items-start absolute mt-110 ml-20 gap-2">
+          <h1 className="text-white text-6xl">Your Gateway to The Cosmos</h1>
+          <h1 className="text-[#7083A3] text-6xl">Begins Here</h1>
+          <button className="rounded-xl px-6 py-3 mt-2 bg-[#011e38] text-white hover:bg-[#7083A3] hover:text-white duration-300">
+            <h3 className="text-xl">Explore Now</h3>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
